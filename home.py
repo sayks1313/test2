38399f83c57cc28fc92cd00064f13b54a1573553
Andrey Glazkov <sayks1313@mail.ru>
2023-02-17 02:10:30 +0400

    Если 1 и 2 переделал

--- home.py
+++ home.py
@@ -3,12 +3,14 @@
 #     if x % 2 == 0:
 #         print(x)
 
-y = input("Введите 1 или 2:")
-if (2):
-    for y in range(1, 101):
-        if y % 2 == 0:
-            print(y)
-if (1):
-    for y in range(1, 101):
-        if y % 2 == 1:
-            print(y)
+y = int(input("Введите 1 или 2:"))
+if y == 2:
+    for x in range(1, 101):
+        if x % 2 == 0:
+            print(x)
+elif y == 1:
+    for a in range(1, 101):
+        if a % 2 == 1:
+            print(a)
+else:
+    print("Фигню написал")
