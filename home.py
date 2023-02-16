4306f9c8589df3bd614789b3110750405a0e582b
Andrey Glazkov <sayks1313@mail.ru>
2023-02-17 01:08:38 +0400

    Если 1 то четные если 2 то нечетные

--- home.py
+++ home.py
@@ -1,4 +1,14 @@
 # #Привет
-for x in range(1, 101):
-    if x % 2 == 0:
-        print(x)
+# for x in range(1, 101):
+#     if x % 2 == 0:
+#         print(x)
+
+y = input("Введите 1 или 2:")
+if (2):
+    for y in range(1, 101):
+        if y % 2 == 0:
+            print(y)
+if (1):
+    for y in range(1, 101):
+        if y % 2 == 1:
+            print(y)
