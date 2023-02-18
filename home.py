526ef51b84cc65d00dde37f4d7d157a56a3c9317
Andrey Glazkov <sayks1313@mail.ru>
2023-02-19 01:37:29 +0400

    Четные в массиве

--- home.py
+++ home.py
@@ -3,14 +3,15 @@
 #     if x % 2 == 0:
 #         print(x)
 
-y = int(input("Введите 1 или 2:"))
-if y == 2:
-    for x in range(1, 101):
-        if x % 2 == 0:
-            print(x)
-elif y == 1:
-    for a in range(1, 101):
-        if a % 2 == 1:
-            print(a)
-else:
-    print("Фигню написал")
+# y = int(input("Введите 1 или 2:"))
+# if y == 2:
+#     for x in range(1, 101):
+#         if x % 2 == 0:
+#             print(x)
+# elif y == 1:
+#     for a in range(1, 101):
+#         if a % 2 == 1:
+#             print(a)
+# else:
+#     print("Фигню написал")
+
---
+++ massiv.py
@@ -0,0 +1,5 @@
+from array import *
+a = [1,2,3,4,5,6,7,8,9,10]
+for x in a:
+    if x % 2 == 0:
+        print (x)
